09f6cacde09b7c0fc80f61f2528dd0407e4f85dc
minhpvwh <wh.pvminh@gmail.com>
2018-12-05 08:41:29 +0700

    'update'

--- Authen/Login/urls.py
+++ Authen/Login/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.ClassIndex.as_view(), name="index"),
     path('login/', views.LoginClass.as_view(), name="login"),
     path('view-user/', views.ViewUser.as_view(), name="user_view"),
-    path('view-product/', views.view_product,name='view_product'),
+    path('view-product/', views.view_product, name='view_product'),
+    path('add-post/', views.AddPost.as_view(), name="add_post"),
 ]
--- Authen/Login/views.py
+++ Authen/Login/views.py
@@ -3,6 +3,7 @@ from django.views import View
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, decorators
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import PostForm
 
 
 # Create your views here.
@@ -36,3 +37,21 @@ class ViewUser(LoginRequiredMixin, View):
 @decorators.login_required(login_url='/login/login')
 def view_product(request):
     return HttpResponse("Xem san pham ")
+
+
+class AddPost(View):
+    def get(self, request):
+        a = PostForm()
+        return render(request, 'Login/add_post.html', {'fm': a})
+
+    def post(self, request):
+        a = PostForm(request.POST)
+        if not a.is_valid():
+            return HttpResponse("nhap sai du lieu r")
+        # cache reload tu database user
+        print(request.user.get_all_permissions)
+        if request.user.has_perm('Login.add_post'):
+            a.save()
+        else:
+            return HttpResponse("may k có quyèn")
+        return HttpResponse("oke")
